b75fe7fcf9f9294cdbecc56224b7d76cf0d52b07
Quarterpie3141 <prashan.csgo@gmail.com>
2024-08-18 11:24:26 +0800

    added dependencies for kubernetes install

--- kubernetes-install.sh
+++ kubernetes-install.sh
@@ -318,6 +318,8 @@ fi
 if [[ "$cri" != "other" ]]; then
     sleep 1
     print_info "Installing Kubernetes..." 1
+    sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates gpg
+    sudo apt update
     curl -fsSL https://pkgs.k8s.io/core:/stable:/$kubernetes_version/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
     echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$kubernetes_version/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
     sudo apt update
