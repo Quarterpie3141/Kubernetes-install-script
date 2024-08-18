6826e3c84061322cc1fcf75a2d3d46c53634aa80
Quarterpie3141 <prashan.csgo@gmail.com>
2024-08-18 09:44:24 +0800

    changes

--- install.sh
+++ install.sh
@@ -6,72 +6,144 @@ print_info() {
     if [ "$2" -eq 1 ]; then
         printf "\n\n\n"
     fi
-    tput setab 6 && tput setaf 0
+    tput smul
+    tput setab 0 && tput setaf 6
     printf "$1"
     tput setab 0 && tput setaf 7
+    tput rmul
     printf "\n"
 }
 
 print_success() {
-    tput setab 2 && tput setaf 7
+    tput setab 0 && tput setaf 2
     printf "$1"
     tput setab 0 && tput setaf 7
     printf "\n"
 }
 
+print_warn() {
+    tput setab 0 && tput setaf 3
+    printf "$1"
+    tput setab 0 && tput setaf 0
+    printf "\n"
+}
+
+
+
 echo "Starting install script"
-
-if [[ "$is_control_plane" =~ ^([nN][oO]?|[nN])$ ]]; then
-fi
-read -p "Is this node a control plane(Y/n):" is_control_plane
+tput smul
+read -p "Is this node a control plane(Y/n): " is_control_plane
+tput rmul
 is_control_plane=${is_control_plane:-Y}
+
 if [[ "$is_control_plane" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    read -p "did you want to set up a control plane endpoint [--control-plane-endpoint]? (y/N):" control_plane_endpoint
+    tput smul
+    read -p "Did you want to set up a control plane endpoint [--control-plane-endpoint]? (y/N): " control_plane_endpoint
+    tput rmul
     control_plane_endpoint=${control_plane_endpoint:-N}
-    if [[ "$is_control_plane" =~ ^([nN][oO]?|[nN])$ ]]; then
-       read -p "enter your endpoint address (ip address, domain name)" endpoint_address
+    if [[ "$control_plane_endpoint" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        tput smul
+        read -p "Enter your endpoint address: " endpoint_address
+        tput rmul
     fi
+else
+    while [ -z "$control_plane_address" ]; do
+        tput smul
+        read -p "What is your control plane address and port [<ip>:<port> | <fqdn>:<port>]? " control_plane_address
+        tput rmul
+        if [ -z "$control_plane_address" ]; then
+            tput smul
+            echo "Control plane address cannot be empty. Please enter a IP or FQDN."
+            tput rmul
+        fi
+    done
+
+    while [ -z "$cluster_token" ]; do
+        tput smul    
+        read -p "What is your existing cluster token [kubeadm token create]? " cluster_token
+        tput rmul
+        if [ -z "$cluster_token" ]; then
+            print_warn "Cluster token cannot be empty. Please enter a token."
+        fi
+    done
 fi
-read -p "what version of kubernates are you using(v1.31)" kubernetes_version
+tput smul
+read -p "What version of Kubernetes are you using(v1.31): " kubernetes_version
 kubernetes_version=${kubernetes_version:-v1.31}
-
-read -p "what container runtime are you using[cri-o | docker | containerd](cri-o)" container_runtime
-echo "kubernetes will use the container runtime interface CRI, by default,if you are using the docker container runtime, please manually configure the cri-dockerd CRI"
-
+tput rmul
+tput smul
+read -p "What container runtime are you using [cri-o | containerd | other](cri-o): " container_runtime
+tput rmul
 container_runtime=${container_runtime:-cri-o}
+
 case "$container_runtime" in
     cri-o)
         echo "You selected CRI-O as the container runtime."
-        read -p "What version of CRI-O are you using(v1.30)" cri_o_vers
-        ;;
-    docker)
-        echo "You selected Docker as the container runtime."
-        echo "docker is not support yet, you will have to manually install docker and dokerd-cni manually"
+        read -p "What version of CRI-O are you using (v1.30): " cri_o_vers
+        cri_o_vers=${cri_o_vers:-v1.30}
         ;;
     containerd)
         echo "You selected Containerd as the container runtime."
-        echo "containered is not supported yet, you will have to install containerd manually"
+        ;;
+    other)
+        echo "You will have to manually install your chosen container runtime."
+        echo "If you are using Docker, please manually configure the cri-dockerd CRI."
         ;;
     *)
-        echo "Invalid input. Please choose between cri-o, docker, or containerd."
+        echo "Invalid input. Please choose between cri-o, other, or containerd."
         exit 1  
         ;;
 esac
 
-
 if [[ "$is_control_plane" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    read -p "what container container network interface(CNI) are you using [calcio | other](calico):" cni
+    read -p "What container network interface (CNI) are you using [calico | other](calico): " cni
     cni=${cni:-calico}
-    read -p "what is your pod network CIDR[--pod-network-cidr] (192.168.0.0/16)" cni_cidr
-    cni_cidr=${cni_cidr:-192.168.0.0/16}
-    read -p "did you want to taint this control plane?(y/N)" is_tainted
+    if [[ "$cni" =~ ^(other)$ ]]; then
+        echo "You will have to manually install your desired CNI after this script finishes."
+    else
+        read -p "What version of Calico are you using? (v3.28.1): " calico_vers
+        calico_vers=${calico_vers:-v3.28.1}
+        read -p "What is your pod network CIDR [--pod-network-cidr] (192.168.0.0/16): " cni_cidr
+        cni_cidr=${cni_cidr:-192.168.0.0/16}
+    fi
+    read -p "Did you want to taint this control plane? (y/N): " is_tainted
     is_tainted=${is_tainted:-N}
 fi
-read -p "what are your desired node ports?(30000:32767/tcp)" node_ports
+
+read -p "What are your desired node ports? (30000:32767/tcp): " node_ports
 node_ports=${node_ports:-30000:32767/tcp}
 
+print_all_values() {
+    echo "Are the following values correct?:"
+    echo "Is control plane: $is_control_plane"
+    if [[ "$is_control_plane" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        echo "Control plane endpoint: $control_plane_endpoint"
+        if [[ "$control_plane_endpoint" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+            echo "Endpoint address: $endpoint_address"
+        fi
+        echo "CNI: $cni"
+        if [[ "$cni" != "calico" ]]; then
+            echo "CNI CIDR: $cni_cidr"
+        fi
+        echo "Is tainted: $is_tainted"
+    fi
+    echo "Kubernetes version: $kubernetes_version"
+    echo "Container runtime: $container_runtime"
+    if [[ "$container_runtime" == "cri-o" ]]; then
+        echo "CRI-O version: $cri_o_vers"
+    fi
+    echo "Node ports: $node_ports"
+}
 
-printf "Starting the installation process in \n"
+print_all_values
+
+read -p "(Y/n): Are the above values correct? " final_check
+
+if [[ "$final_check" =~ ^([nN][oO]|[nN])$ ]]; then
+    exit 1
+fi
+
+printf "Starting the installation process in\n"
 for i in {1..5}; do
     printf "$((6-i))"
     for j in {1..3}; do
@@ -115,7 +187,7 @@ print_success "Done!"
 
 sleep 1
 
-print_info "Enabling IPv4 packet forwarding" 1
+print_info "Enabling IPv4 packet forwarding..." 1
 if sysctl net.ipv4.ip_forward | grep -q 'net.ipv4.ip_forward = 1'; then
     print_success "IPv4 packet forwarding is already enabled."
 else
@@ -127,81 +199,123 @@ print_success "Done!"
 
 sleep 1
 
-print_info "Installing UFW" 1
+print_info "Installing UFW..." 1
 sudo apt install -y ufw
 print_success "Done!"
 
 sleep 1
 
-print_info "Setting up firewall settings" 1
+print_info "Setting up firewall settings..." 1
 printf "\n"
-print_info "Allowing Kubernetes API server" 0
+print_info "Allowing Kubernetes API server..." 0
 sudo ufw allow 6443/tcp
 printf "\n"
 sleep 0.5
-print_info "Allowing etcd server client API" 0
+print_info "Allowing etcd server client API..." 0
 sudo ufw allow 2379:2380/tcp
 printf "\n"
 sleep 0.5
-print_info "Allowing Kubelet API" 0
+print_info "Allowing Kubelet API..." 0
 sudo ufw allow 10250/tcp
 printf "\n"
 sleep 0.5
-print_info "Allowing kube-scheduler" 0
+print_info "Allowing kube-scheduler..." 0
 sudo ufw allow 10259/tcp
 printf "\n"
 sleep 0.5
-print_info "Allowing kube-controller-manager" 0
+print_info "Allowing kube-controller-manager..." 0
 sudo ufw allow 10257/tcp
 printf "\n"
 sleep 0.5
-print_info "Allowing kube-proxy" 0
+print_info "Allowing kube-proxy..." 0
 sudo ufw allow 10256/tcp
 printf "\n"
 sleep 0.5
-#if [[ "$cni" =~ ^(calico)$ ]]; then
-    print_info "Allowing BIRD" 0
+if [[ "$cni" =~ ^(calico)$ ]]; then
+    print_info "Allowing BIRD..." 0
     sudo ufw allow 179/tcp
     printf "\n"
     sleep 0.5
-    print_info "Allowing Typha" 0
+    print_info "Allowing Typha..." 0
     sudo ufw allow 5473/tcp
     printf "\n"
     sleep 0.5
-#fi
-print_info "Allowing NodePort Services" 0
-sudo ufw allow 30000:32767/tcp
+fi
+print_info "Allowing NodePort Services..." 0
+sudo ufw allow "$node_ports"
 print_success "Done!"
 sudo ufw enable
 
 sleep 1
 
-print_info "Installing CRI-O" 1
-sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates gpg
-sudo curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
-sudo apt-get update
-sudo apt-get install -y cri-o
-print_info "Enabling CRI-O" 0
-sleep 1
-sudo systemctl start crio.service
-print_success "Done!"
+if $cri = crio; then
+    print_info "Installing CRI-O..." 1
+    sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates gpg
+    sudo curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$cri_o_vers/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$cri_o_vers/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
+    sudo apt-get update
+    sudo apt-get install -y cri-o
+    print_info "Enabling CRI-O..." 0
+    sleep 1
+    sudo systemctl start crio.service
+    print_success "Done!"
 
-sleep 1
+elif $cri = containerd; then
+    sudo apt-get install -y software-properties-common curl apt-transport-https ca-certificates gpg
+    sudo apt-get update
+    sudo apt-get install -y containerd
+    print_info "Enabling containerd..." 0
+    sleep 1
+    sudo systemctl start containerd.service
+    print_success "Done!"
+fi
 
-print_info "Installing Kubernetes" 1
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt update
-sudo apt-get install -y kubelet kubeadm kubectl
-sleep 1
-sudo apt-mark hold kubelet kubeadm kubectl
-sleep 1
-sudo systemctl enable --now kubelet
-print_success "Done!"
+if [[ "$cri" != "other" ]]; then
+    sleep 1
+    print_info "Installing Kubernetes..." 1
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/$kubernetes_version/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$kubernetes_version/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    sudo apt update
+    sudo apt-get install -y kubelet kubeadm kubectl
+    sleep 1
+    sudo apt-mark hold kubelet kubeadm kubectl
+    sleep 1
+    sudo systemctl enable --now kubelet
+    print_success "Done!"
 
-print_info "Pulling required config images, this may take a few minutes" 1
-kubeadm config images pull 
-print_success "Done!"
+    print_info "Pulling required config images, this may take a few minutes..." 1
+    kubeadm config images pull 
+    print_success "Done!"
 
-print_success "Kubeadm is ready to use"
+    print_success "Kubeadm is ready to use"
+
+    if [[ "$is_control_plane" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        echo "Initializing cluster..."
+        kubeadm init --control-plane-endpoint "$endpoint_address" --pod-network-cidr "$cni_cidr"
+        echo "Cluster initialization complete."
+
+        echo "Running post-init script..."
+        mkdir -p $HOME/.kube
+        sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+        sudo chown $(id -u):$(id -g) $HOME/.kube/config
+        echo "Post-init script complete."
+
+        if [[ "$is_tainted" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+            kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+        fi
+
+        if [[ "$cni" == "calico" ]]; then
+            kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/$calico_vers/manifests/tigera-operator.yaml
+            kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/$calico_vers/manifests/custom-resources.yaml
+        fi
+    else
+        ca_cert=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
+        kubeadm join "$control_plane_address" --token "$cluster_token" --discovery-token-ca-cert-hash sha256:"$ca_cert"
+    fi
+
+    kubectl get nodes -o wide
+
+    echo "Your Kubernetes node has been initialized with no errors."
+elif
+    echo "Your node is ready to have kubernetes installed"
+fi
